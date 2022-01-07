8d6c4d6e523bb1a56cd710eb7af87d80a6ae3ab1
yang_c602 <15988815465@163.com>
2022-01-07 16:51:29 +0800

    链表专练

--- src/main/java/链表/两两交换链表中的节点/cdoe.java
+++ src/main/java/链表/两两交换链表中的节点/cdoe.java
@@ -7,7 +7,28 @@ public class cdoe {
 
     }
     public ListNode swapPairs(ListNode head) {
-
-        return null;
+        if(head==null)
+            return null;
+        if(head.next==null)
+            return head;
+        ListNode a=head;
+        ListNode b=head.next;
+        ListNode c=b.next;
+        head=b;
+        b.next=a;
+        while(c!=null){
+            if(c.next!=null) {
+                a.next = c.next;
+            }else{
+                a.next=c;
+                return head;
+            }
+            a=c;
+            b=c.next;
+            c=c.next.next;
+            b.next=a;
+        }
+        a.next=null;
+        return head;
     }
 }
