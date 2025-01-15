82961fb4e273a6bf9e4073d8b6a6ab876934ef1c
yc-super <15988815465@163.com>
2025-01-15 21:18:10 +0800

    LeetCode 2067. 等计数子串的数量。滑动窗口

---
+++ src/main/java/大厂算法题2025/等计数子串的数量/Solution.java
@@ -0,0 +1,95 @@
+package 大厂算法题2025.等计数子串的数量;
+
+import java.util.HashSet;
+
+public class Solution {
+    public static void main(String[] args) {
+        int method = new Solution().method("aaabcbbcc", 3);
+        System.out.println(method);
+    }
+
+    // 这种方法只用了2个限制，不能很好地处理遍历过程中某个字母数目已经大于count，因为没有记录各个字母出现的次数
+    private int method(String s, int count) {
+        if (count > s.length())
+            return 0;
+//        int len = s.length();
+        char[] charArray = s.toCharArray();
+        // 前缀和，记录26个字母从0到下标i的数量
+//        int[][] array=new int[26][len];
+//        array[charArray[0]-'a'][0]=1;
+//        for (int i = 1; i < charArray.length; i++) {
+//            array[charArray[i]-'a'][i]=array[charArray[i]-'a'][i-1]+1;
+//        }
+
+        int result = 0;
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < charArray.length - count + 1; i++) {
+            for (int j = i + count - 1; j < charArray.length; j++) {
+                if (j == i + count - 1) {
+                    set.clear();
+                    for (int k = i; k <= j; k++) {
+                        set.add(charArray[k]);
+                    }
+                } else {
+                    set.add(charArray[j]);
+                }
+                // 检查区间是否是count的倍数
+                if ((j - i + 1) % count != 0)
+                    continue;
+                // 检查字母种类乘以count是否等于区间
+                if (set.size() * count != (j - i + 1))
+                    continue;
+                result++;
+            }
+        }
+        return result;
+    }
+
+    // 方法2，滑动窗口，遍历的同时记录出现的次数，同时进行左指针右移
+    public int equalCountSubstrings(String s, int count) {
+        int n = s.length();
+        if (n < count) {
+            return 0;
+        }
+        int ans = 0;
+        // 等计数子串的长度为count的整数倍，有多少不同的字母，其长度就是count的多少倍
+        // 最多有26种字母，所以长度最多为count的26倍
+        for (int i = 1; i * count <= n && i <= 26; i++) {
+            int len = i * count;
+            int left = 0;
+            int right = 0;
+            // 记录窗口内各种字母的数量
+            int[] window = new int[26];
+            // 记录窗口内 字母种类 的数量
+            int valid = 0;
+            while (right < n) {
+                if (window[s.charAt(right) - 'a'] == 0) {
+                    valid++;
+                }
+                window[s.charAt(right) - 'a']++;
+                // 如果滑动窗口内的某个字母数量超过count，则此窗口已经不符合要求，左指针右移
+                while (window[s.charAt(right) - 'a'] > count) {
+                    window[s.charAt(left) - 'a']--;
+                    if (window[s.charAt(left) - 'a'] == 0) {
+                        valid--;
+                    }
+                    left++;
+                }
+                // 窗口长度为i * count时，检查其字母种类数是否为i
+                if (right - left + 1 == len) {
+                    if (valid == i) {
+                        ans++;
+                    }
+                    window[s.charAt(left) - 'a']--;
+                    if (window[s.charAt(left) - 'a'] == 0) {
+                        valid--;
+                    }
+                    left++;
+                }
+                right++;
+            }
+        }
+        return ans;
+    }
+
+}
